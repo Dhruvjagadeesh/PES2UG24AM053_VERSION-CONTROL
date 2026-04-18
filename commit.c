12cdcd2203ff8ce82a190ceabfeed7315b459d55
Dhruvjagadeesh <dhruvjagadeesh1@gmail.com>
2026-04-18 11:03:30 +0530

    Phase 4: add parent commit reading from HEAD with has_parent flag

--- commit.c
+++ commit.c
@@ -14,6 +14,7 @@
 // PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
 // Phase 4: tree building in commit_create
+// Phase 4: parent commit reading from HEAD
 #include "commit.h"
 #include "index.h"
 #include "tree.h"
